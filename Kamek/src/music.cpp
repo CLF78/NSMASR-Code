43e07346a526da5751b830f553a1dbee76750a2e
H1dd3nM1nd <twomp021@gmail.com>
2020-11-15 11:53:40 +0100

    Improved music patch and removed unused code

--- Kamek/src/music.cpp
+++ Kamek/src/music.cpp
@@ -1,138 +1,37 @@
-#include <game.h>
-#include <sfx.h>
 #include "music.h"
 
-struct HijackedStream {
-	//const char *original;
-	//const char *originalFast;
-	u32 stringOffset;
-	u32 stringOffsetFast;
-	u32 infoOffset;
-	u8 originalID;
-	int streamID;
+// name, hasFast, hasDrums
+const CustomStream SongNameList [] = {
+	{"SMBGRASS", true, false},
+	{"SMBUNDERGROUND", true, false},
+	{"SMBUNDERWATER", true, false},
+	{"SMBBONUS", true, false},
+	{"SMBCASTLE", true, false},
+	{"INTRO", false, false},
+	{"SMBSNOW", true, false},
+	{"SMBSNOWNIGHT", true, false},
+	{"SMBGRASSNIGHT", true, false},
+	{NULL, NULL, NULL}
 };
 
-struct Hijacker {
-	HijackedStream stream[2];
-	u8 currentStream;
-	u8 currentCustomTheme;
-};
-
-
-
-const char* SongNameList [] = {
-	"SMBGRASS",
-	"SMBUNDERGROUND",
-	"SMBUNDERWATER",
-	"SMBBONUS",
-	"SMBCASTLE",
-	"INTRO",
-	"SMBSNOW",
-	"SMBSNOWNIGHT",
-	"SMBGRASSNIGHT",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",	
-	"",	
-	"",	
-	"",	
-	"",	
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	NULL	
-};
-
-
-
-// Offsets are from the start of the INFO block, not the start of the brsar.
-// INFO begins at 0x212C0, so that has to be subtracted from absolute offsets
-// within the brsar.
-
-#define _I(offs) ((offs)-0x212C0)
-
-Hijacker Hijackers[2] = {
-	{
-		{
-			{/*"athletic_lr.n.32.brstm", "athletic_fast_lr.n.32.brstm",*/ _I(0x4A8F8), _I(0x4A938), _I(0x476C4), 4, STRM_BGM_ATHLETIC},
-			{/*"BGM_SIRO.32.brstm", "BGM_SIRO_fast.32.brstm",*/ _I(0x4B2E8), _I(0x4B320), _I(0x48164), 10, STRM_BGM_SHIRO}
-		},
-		0, 0
-	},
-
-	{
-		{
-			{/*"STRM_BGM_CHIJOU.brstm", "STRM_BGM_CHIJOU_FAST.brstm",*/ _I(0x4A83C), _I(0x4A8B4), 0, 1, STRM_BGM_CHIJOU},
-			{/*"STRM_BGM_CHIKA.brstm", "STRM_BGM_CHIKA_FAST.brstm",*/ _I(0x4A878), _I(0x4A780), 0, 2, STRM_BGM_CHIKA},
-		},
-		0, 0
-	}
-};
-
-extern void *SoundRelatedClass;
 inline char *BrsarInfoOffset(u32 offset) {
 	return (char*)(*(u32*)(((u32)SoundRelatedClass) + 0x5CC)) + offset;
 }
 
-void FixFilesize(u32 streamNameOffset);
-
 u8 hijackMusicWithSongName(const char *songName, int themeID, bool hasFast, int channelCount, int trackCount, int *wantRealStreamID) {
 	Hijacker *hj = &Hijackers[channelCount==4?1:0];
 
-	// do we already have this theme in this slot?
-	// if so, don't switch streams
-	// if we do, NSMBW will think it's a different song, and restart it ...
-	// but if it's just an area transition where both areas are using the same
-	// song, we don't want that
+	// If it's just an area transition where both areas are using the same song, we don't want the music to restart
 	if ((themeID >= 0) && hj->currentCustomTheme == themeID)
 		return hj->stream[hj->currentStream].originalID;
 
-	// which one do we use this time...?
+	// Which one do we use this time...?
 	int toUse = (hj->currentStream + 1) & 1;
 
 	hj->currentStream = toUse;
 	hj->currentCustomTheme = themeID;
 
-	// write the stream's info
+	// Write the stream's info
 	HijackedStream *stream = &hj->stream[hj->currentStream];
 
 	if (stream->infoOffset) {
@@ -147,49 +46,21 @@ u8 hijackMusicWithSongName(const char *songName, int themeID, bool hasFast, int 
 	sprintf(BrsarInfoOffset(stream->stringOffset), "stream/%s.brstm", songName);
 	sprintf(BrsarInfoOffset(stream->stringOffsetFast), hasFast?"stream/%s_F.brstm":"stream/%s.brstm", songName);
 
-	// update filesizes
-	FixFilesize(stream->stringOffset);
-	FixFilesize(stream->stringOffsetFast);
-
-	// done!
+	// Done!
 	if (wantRealStreamID)
 		*wantRealStreamID = stream->streamID;
 
 	return stream->originalID;
 }
 
-
-//oh for fuck's sake
-#include "fileload.h"
-//#include <rvl/dvd.h>
-
-void FixFilesize(u32 streamNameOffset) {
-	char *streamName = BrsarInfoOffset(streamNameOffset);
-
-	char nameWithSound[80];
-	snprintf(nameWithSound, 79, "/Sound/%s", streamName);
-
-	s32 entryNum;
-	DVDHandle info;
-	
-	if ((entryNum = DVDConvertPathToEntrynum(nameWithSound)) >= 0) {
-		if (DVDFastOpen(entryNum, &info)) {
-			u32 *lengthPtr = (u32*)(streamName - 0x1C);
-			*lengthPtr = info.length;
-		}
-	} else
-		OSReport("What, I couldn't find \"%s\" :(\n", nameWithSound);
-}
-
-
-
-extern "C" u8 after_course_getMusicForZone(u8 realThemeID) {
+u8 GetMusicForZone(u8 realThemeID) {
 	if (realThemeID < 100)
 		return realThemeID;
 
-	bool usesDrums = (realThemeID >= 200);
-	const char *name = SongNameList[realThemeID - (usesDrums ? 200 : 100)];
-	return hijackMusicWithSongName(name, realThemeID, true, usesDrums?4:2, usesDrums?2:1, 0);
+	// Failsafe that checks the size of the song list to avoid invalid IDs
+	if (realThemeID - 100 > (sizeof(SongNameList) / sizeof(CustomStream)) - 2)
+		return 0;
+
+	CustomStream entry = SongNameList[realThemeID - 100];
+	return hijackMusicWithSongName(entry.name, realThemeID, entry.hasFast, entry.hasDrums?4:2, entry.hasDrums?2:1, 0);
 }
-
-
--- Kamek/src/music.h
+++ Kamek/src/music.h
@@ -1,4 +1,42 @@
 #ifndef MUSIC_H
-#define MUSIC_H 
-u8 hijackMusicWithSongName(const char *songName, int themeID, bool hasFast, int channelCount, int trackCount, int *wantRealStreamID);
+#define MUSIC_H
+#include <game.h>
+#include <sfx.h>
+#include "fileload.h"
+
+extern void *SoundRelatedClass;
+
+struct HijackedStream {
+	u32 stringOffset;
+	u32 stringOffsetFast;
+	u32 infoOffset;
+	u8 originalID;
+	int streamID;
+};
+
+struct Hijacker {
+	HijackedStream stream[2];
+	u8 currentStream;
+	u8 currentCustomTheme;
+};
+
+struct CustomStream {
+	const char* name;
+	bool hasFast;
+	bool hasDrums;
+};
+
+// The following offsets are from the start of the INFO block, not the start of the brsar.
+// The first couple is for two-channel songs and the other is for four-channel ones
+
+HijackedStream Athletic = {0x29638, 0x29678, 0x26404, 4, STRM_BGM_ATHLETIC};
+HijackedStream Shiro = {0x2A028, 0x2A060, 0x26EA4, 10, STRM_BGM_SHIRO};
+HijackedStream Chijou = {0x2957C, 0x295F4, 0x29560, 1, STRM_BGM_CHIJOU};
+HijackedStream Chika = {0x295B8, 0x294C0, 0x2959C, 2, STRM_BGM_CHIKA};
+
+Hijacker Hijackers[2] = {
+	{Athletic, Shiro, 0, 0},
+	{Chijou, Chika, 0, 0}
+};
+
 #endif /* MUSIC_H */
